b8ce8c864e80d9f1159edc2f380d332deaa577b5
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-07-23 00:11:58 +0530

    logging the backend code to fix 401 error

--- backend/middleware.py
+++ backend/middleware.py
@@ -3,6 +3,8 @@ from auth import verify_token
 
 def get_current_user_id(request: Request):
     token = request.cookies.get("access_token")
+    
+    print("access_token cookie:", token)
     if not token:
         raise HTTPException(status_code=401, detail="Not authenticated")
     user_id = verify_token(token)
