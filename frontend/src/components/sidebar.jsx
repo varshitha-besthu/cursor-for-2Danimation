47fc3b296d14e6d41a3703fa474527d1fc408d43
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-08-08 21:51:32 +0530

    Added the picture

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -21,7 +21,7 @@ export default function Sidebar(){
     const picture = useRecoilValue(pictureAtom);
 
     useEffect(() => {
-        if(picture == ""){
+        if(picture == "hello guru kosame ra jeeveihtam"){
           picture = "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
         }
       },[])
